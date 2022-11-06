9c957b8fbe778c9bbd44f0f70491af94fe178ab3
MrGogoll <115408973+MrGogoll@users.noreply.github.com>
2022-11-06 15:02:21 +0300

    10

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int summ;
+    summ = a + b + c;
+    if (summ % 10 == 6 && summ % 2 == 0 || summ % 10 == 9 && summ % 2 != 0) {
+        cout << summ << " magic";
+    }
+   
+    else  {
+        cout << summ << " NO magic";
+    }
+}
+
